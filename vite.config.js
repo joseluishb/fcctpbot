8bdf3d030810fb023bb39fe160d3e980251d4ae6
jose luis <joseluishube@gmail.com>
2024-06-06 15:05:00 +0000

    lara and botman/botman botman/web-driver

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    server: {
+        hmr: {
+            host: 'localhost',
+            strictPort: true,
+            port: 5176
+        },
+    },
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
