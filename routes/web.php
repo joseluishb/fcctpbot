813f7bcef3d91a45ebf7bc57bef7d96cdcf6484d
jose luis <joseluishube@gmail.com>
2024-06-21 18:25:58 +0000

    breeze + livewire

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,30 @@
 <?php
 
-use App\Models\SapM\Cliente;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BotManController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 Route::match(['get', 'post'], '/botman', [BotManController::class, 'handle']);
-
-Route::get('/cliente', function () {
-    $nroDoc = '74233592';
-
-    $cliente = Cliente::where('dni', $nroDoc)->first();
-
-
+Route::get('/landing', function () {
+    return view('landing');
 });
 
 Route::get('/demo', function () {
     return view('demo');
 });
 
-Route::get('/landing', function () {
-    return view('landing');
-});
+require __DIR__.'/auth.php';
