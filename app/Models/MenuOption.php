fdec0c33df0cad1c931ffb8d23416fed64646256
jose luis <joseluishube@gmail.com>
2024-07-02 00:41:42 +0000

    added softdelete in MenuOption

--- app/Models/MenuOption.php
+++ app/Models/MenuOption.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MenuOption extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['desc_opcion', 'respuesta'];
 
     public function optionRoute()
