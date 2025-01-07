cfba9fde09c1dcbcfcd2c3bde703f98f8983b786
jose luis <joseluishube@gmail.com>
2025-01-07 12:38:25 +0000

    dialogflow mas info verano

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -881,7 +881,7 @@
         }
     }
         </script>
-{{--         <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
- --}}</body>
+        <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
+</body>
 
 </html>
