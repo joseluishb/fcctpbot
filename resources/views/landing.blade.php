9380c5329a18ad931d3faf0bc33c6f8c5d4259e5
José Luis H <joseluishube@gmail.com>
2025-07-04 11:02:15 -0500

    calendario aca update

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -257,7 +257,7 @@ $shows = explode(',', request('show', '')); // Si no existe, devuelve un array v
                             <h2 class="subtitulo leading-5 pb-2 pe-10">Calendario académico</h2>
                             <p class="text-gray-500">Semestre 2025-II</p>
                             <div class="mt-10 mb-4">
-                                <a href=" {{ url('documentos/calendario-academico-2025-II.pdf') }}" target="_blank"
+                                <a href="https://matricula.fcctp.edu.pe/documentos/Calendario-Academico-2025-II.pdf" target="_blank"
                                     class="bg-gray-600 hover:bg-gray-700 text-white border px-4 py-3 rounded-md transition-colors duration-300 ease-in-out">
                                     Ver Documento <i class="pl-1 fa-regular fa-file-lines"></i>
                                 </a>
